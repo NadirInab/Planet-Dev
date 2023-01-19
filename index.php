aa9212cc4932fe1ee423cd27319ac6665f9fbacb
nadir inab <nadir.inab.dev@gmail.com>
2023-01-19 17:18:39 +0100

    dashboard is fine and added

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php 
+    require "./classes/DbConnection.php" ;
     require "./services/adminServices.php" ;
     require "./templates/header.php" ;
 
     if(isset($_POST["submit"])){
         addArticle() ;
-        // dd($_POST) ;
-        // die() ;
     }
 ?>
 <main class="w-9/12 mx-auto ">
+    <div class="flex flex-wrap">
+        <?php  require "./templates/articles.php" ;?>
+    </div>
     <?php require "./templates/form.php" ;?>
 </main>
 
