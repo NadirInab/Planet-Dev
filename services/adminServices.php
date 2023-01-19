d282e7f4b95b9e0e486b2d0d14f3f74703177d5c
nadir inab <nadir.inab.dev@gmail.com>
2023-01-19 17:19:07 +0100

    admin services are added

--- services/adminServices.php
+++ services/adminServices.php
@@ -4,9 +4,9 @@ require "./include/autoload.php";
 
 $connection = new DbConnection() ;
 $connect = $connection->connect() ;
+
 function addArticle()
 {
     global $connect;
-    // $articleData = ["title" => $_POST["title"], "image" => $_POST["image"], "body" => $_POST];
     AdminCrud::addArticle($_POST, $connect);
 }
