bda5b8c572071f2b14ad84f42436ba8e9137cf35
nadir inab <nadir.inab.dev@gmail.com>
2023-01-19 17:19:39 +0100

    admin crud code is added

--- classes/AdminCrud.php
+++ classes/AdminCrud.php
@@ -1,15 +1,24 @@
 <?php  
     class AdminCrud {
         public static function addArticle($data, $con){
-            // dd($_POST) ;
-            // die() ;
-            for($i = 0 ; $i< 2 ; $i++){
                 $query = "INSERT INTO article(title, image, body) VALUES(:title, :image, :body)" ;
                 $stmt = $con->prepare($query) ;
                 $stmt->bindParam('title', $data['title']) ;
                 $stmt->bindParam('image', $data['image']) ;
                 $stmt->bindParam('body', $data['body']) ;
                 $stmt->execute() ;
-            }
+        }
+
+        public function deleteArticles($id, $con){
+            $query = "DELETE FROM article WHERE id= :id" ;
+            $stmt = $con->prepare($query) ;
+            $stmt->bindParam('id', $id) ;
+            $stmt->execute() ;
+        }
+
+        public static function fetchArticles($con){
+            $query = "SELECT * FROM article" ;
+            $stmt = $con->query($query) ;
+            return $stmt->fetchAll() ;
         }
     }
