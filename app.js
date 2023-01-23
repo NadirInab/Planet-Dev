8606e651d713b662506e993f0b39737f9f6ec1b0
nadir inab <nadir.inab.dev@gmail.com>
2023-01-23 17:22:30 +0100

    searching is fine and well working

--- app.js
+++ app.js
@@ -77,6 +77,23 @@ signIn.addEventListener("click", ()=>{
     signInForm.style.display = "block" ;
 })
 
+function searchForArticle(){
+    let input, filter;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    cards = document.getElementsByClassName("card")
+    titles = document.getElementsByClassName("card-title");
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < cards.length; i++) {
+      a = titles[i];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  }
 // anotherForm.addEventListener('click', ()=>{
 //     let div = document.createElement("div") ;
 //     let div1 = document.createElement("div") ;
