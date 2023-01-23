975a58510a3ac0eff593921e03e885ade619fae3
nadir inab <nadir.inab.dev@gmail.com>
2023-01-23 17:24:40 +0100

    templates are well added

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -7,45 +7,31 @@
     $fictionCounter = 5 ;
     $mystryCounter = 5 ;
 ?>
- <div class="mx-5 card mt-2" style="width:15rem; background-color: #FFF7E9;">
+ <div class="mx-5 card mt-2" style="width:17rem; background-color: #FFF7E9;">
     <img class="card-img-top p-3" src="../images/article.png" alt="Card image cap">
     <div class="card-body">
-        <h4 class="card-title pt-3 mt-3 text-center text-muted">Total of Articles : <strong class="fw-bold text-warning">  <?= $userArticles ;  ?> </strong> </h4>
+        <h4 class="card-title pt-3 mt-3 text-center">Total of Articles : <strong class="fw-bold">  <?= $articlesCount ;  ?> </strong> </h4>
     </div>
 </div>
  <div class="mx-5 card mt-2" style="width:15rem;background-color: #FFF7E9;">
     <img class="card-img-top p-3" src="../images/article.png" alt="Card image cap">
     <div class="card-body">
-        <h4 class="card-title pt-3 mt-3 text-center text-muted">My Articles : <strong class="fw-bold text-warning">  <?= $articlesCount ;  ?> </strong> </h4>
+        <h4 class="card-title pt-3 mt-3 text-center">My Articles : <strong class="fw-bold">  <?= $userArticles ;  ?> </strong> </h4>
     </div>
 </div>
 
 <div class="mx-5 card mt-2" style="width: 15rem;background-color: #FFF7E9;">
     <img class="card-img-top p-3" src="../images/users.png" alt="Card image cap">
     <div class="card-body mt-5">
-        <h4 class="card-title pt-3 mt-3 text-center text-muted">Total of Users <br> <strong class="fw-bold text-warning"> <?=  $userCount ?> </strong>  </h4>
+        <h4 class="card-title pt-3 mt-3 text-center">Total of Users <br> <strong class="fw-bold"> <?=  $userCount ?> </strong>  </h4>
     </div>
 </div>
 
 <div class="mx-5 card mt-2" style="width: 15rem;background-color: #FFF7E9;">
     <img class="card-img-top p-3" src="../images/users.png" alt="Card image cap">
     <div class="card-body mt-5">
-        <h4 class="card-title pt-3 mt-3 text-center text-muted">Total of Users <br> <strong class="fw-bold text-warning"> <?=  $userCount ?> </strong>  </h4>
+        <h4 class="card-title pt-3 mt-3 text-center">Total of Users <br> <strong class="fw-bold"> <?=  $userCount ?> </strong>  </h4>
     </div>
 </div>
 
-
-
- <!-- <div class="mx-5 card mt-2 " style="width: 15rem;">
-    <img class="card-img-top" src="../images/codingBooks.webp" alt="Card image cap">
-    <div class="card-body mt-4">
-        <h4 class="card-title pt-3 mt-3 text-center text-muted">Total of IT Books <br> <strong class="fw-bold text-warning"> <?=  $fictionCounter ?> </strong></h4>
-    </div>
-</div> -->
- <!-- <div class="mx-5 card mt-2" style="width: 14rem;">
-    <img class="card-img-top pt-5" src="../images/mystry.jpeg" alt="Card image cap">
-    <div class="card-body mt-4">
-        <h4 class="card-title pt-3 mt-3 text-center text-muted pt-5">Total of mystry Books <br> <strong class="fw-bold text-warning"> <?=  $mystryCounter ?> </strong></h4>
-    </div>
-</div> -->
 
--- templates/profile.php
+++ templates/profile.php
@@ -9,9 +9,9 @@
             </div>
         <div class="col-md-6">
             <div class="card-body h-75 p-5 ">
-                <h6 class="card-title p-2"> <i class="fa-solid fa-user mx-2"></i>  Name  : <strong class="text-secondary">  <?= $_SESSION["admin"] ?>  </strong>   </h6>
-                <h6 class="card-title p-2"><i class="fa-solid fa-envelope mx-1"></i> Email :<strong class="text-secondary"> <?= $_SESSION["email"]   ?> </strong></h6>
-                <h6 class="card-title p-2"> <i class="fa-solid fa-phone mx-2"></i>  Phone : <strong class="text-secondary ">  <?= 'test'   ?></strong>  </h6>
+                <h6 style="color: #243763;" class="card-title p-2"> <i class="fa-solid fa-user mx-2"></i>  Name  : <strong class="text-secondary">  <?= $_SESSION["admin"] ?>  </strong></h6>
+                <h6 style="color: #243763;" class="card-title p-2"><i class="fa-solid fa-envelope mx-1"></i> Email :<strong class="text-secondary"> <?= $_SESSION["email"]   ?> </strong></h6>
+                <h6 style="color: #243763;" class="card-title p-2"> <i class="fa-solid fa-phone mx-2"></i>  Phone : <strong class="text-secondary ">  <?= 'test'   ?></strong></h6>
             </div>
         </div>
         </div>
