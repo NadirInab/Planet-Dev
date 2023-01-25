48ce236cc318f1440d75160fb329c527ed6d9b70
nadir inab <nadir.inab.dev@gmail.com>
2023-01-25 09:59:19 +0100

    AdminFactory is creating successfully new admins

--- classes/AdminFactory.php
+++ classes/AdminFactory.php
@@ -3,18 +3,7 @@
 class AdminFactory {
     
     public static function createAdmin($connection,$data){
-        // echo "<pre>" ;
-        //     var_dump($data) ;
-        // echo "<pre>" ;
-
-         $Admin1 = new Admin($data["fullName"],$data["email"],$data["profile"],$data["pwd"]) ;
-
-        // echo "<pre>" ;
-        //     var_dump($Admin1) ;
-        // echo "<pre>" ;
-
-        // die() ;
-        // $query = "INSERT INTO admin(email, pwd, fullName,image, phone) VALUES(:email,:pass_word,:fullName,:image,:phone)" ;
+        $Admin1 = new Admin($data["fullName"],$data["email"],$data["profile"],$data["pwd"]) ;
         $query = "INSERT INTO admin(name,email,pwd,image) VALUES(:fullName,:email,:pass_word,:image)" ;
         $stmt = $connection->prepare($query) ;
         $stmt->bindParam(':fullName', $Admin1->fullName) ;
